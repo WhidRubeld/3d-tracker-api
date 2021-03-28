f01bdd999a8c50cdbd7273856604a565959d2d03
Vasilev Gennady <whidrubeld@gmail.com>
2021-03-28 22:36:21 +0300

    upgrade seeder

--- database/seeders/ExampleSeeder.php
+++ database/seeders/ExampleSeeder.php
@@ -20,26 +20,29 @@ class ExampleSeeder extends Seeder
      */
     public function run()
     {
-        $tracker_data = ['color_hex' => 'FF0000'];
-
+        // STEP 1 - RACE
         $location_data = [
             'name' => 'Эверест',
             'latitude' => 27.988056,
             'longitude' => 86.925278,
             'zoom_index' => 12,
         ];
-
         $race_data = [
             'title' => 'Отслеживание Эвереста',
             'started_at' => '2021-03-11 14:30:00',
             'duration' => 120,
         ];
+        $location = Location::factory(1)->create($location_data)->first();
+        $race = Race::factory(1)->create(
+            array_merge(
+                $race_data,
+                ['location_id' => $location->id],
+            )
+        )->first();
 
-        $racer_data = [
-            'name' => 'test',
-        ];
-
-        // 27.981056, 86.895278, 6500
+        // STEP 2 - RACER
+        $racer_tracker_data = ['color_hex' => 'ff0000'];
+        $racer_data = ['name' => 'test'];
         $racer_movements = [
             [
                 'latitude' => 27.981056,
@@ -50,31 +53,134 @@ class ExampleSeeder extends Seeder
                 'bearing' => 160,
                 'generated_at' => '2021-03-11 14:30:01',
             ],
+            [
+                'latitude' => 27.981,
+                'longitude' =>  86.892,
+                'altitude' => 6400,
+                'battery' => 80,
+                'speed' => 200,
+                'bearing' => 160,
+                'generated_at' => '2021-03-11 14:30:03',
+            ],
+            [
+                'latitude' => 27.983,
+                'longitude' => 86.887,
+                'altitude' => 6400,
+                'battery' => 80,
+                'speed' => 200,
+                'bearing' => 160,
+                'generated_at' => '2021-03-11 14:30:05',
+            ],
+            [
+                'latitude' => 27.983,
+                'longitude' =>  86.881,
+                'altitude' => 6200,
+                'battery' => 80,
+                'speed' => 200,
+                'bearing' => 160,
+                'generated_at' => '2021-03-11 14:30:07',
+            ],
+            [
+                'latitude' => 27.995,
+                'longitude' =>  86.875,
+                'altitude' => 5900,
+                'battery' => 80,
+                'speed' => 200,
+                'bearing' => 160,
+                'generated_at' => '2021-03-11 14:30:09',
+            ],
+            [
+                'latitude' => 27.999,
+                'longitude' =>  86.865,
+                'altitude' => 5500,
+                'battery' => 80,
+                'speed' => 200,
+                'bearing' => 160,
+                'generated_at' => '2021-03-11 14:30:11',
+            ],
+            [
+                'latitude' => 27.999,
+                'longitude' =>  86.86,
+                'altitude' => 5300,
+                'battery' => 80,
+                'speed' => 200,
+                'bearing' => 160,
+                'generated_at' => '2021-03-11 14:30:13',
+            ],
         ];
 
-        $tracker = Tracker::factory(1)->create($tracker_data)->first();
-
-        $location = Location::factory(1)->create($location_data)->first();
-
-        $race = Race::factory(1)->create(
-            array_merge(
-                $race_data,
-                ['location_id' => $location->id],
-            )
-        )->first();
+        $racer_tracker = Tracker::factory(1)->create($racer_tracker_data)->first();
 
         Racer::factory(1)->create(
             array_merge(
                 $racer_data,
-                ['tracker_id' => $tracker->id, 'race_id' => $race->id]
+                ['tracker_id' => $racer_tracker->id, 'race_id' => $race->id]
             )
         );
 
         foreach ($racer_movements as $movement) {
             TrackerMovement::factory(1)->create(array_merge(
                 $movement,
-                ['tracker_id' => $tracker->id],
+                ['tracker_id' => $racer_tracker->id],
             ));
         }
+
+        // STEP 3 - FLAGS
+        $flag_1_tracker_data = ['color_hex' => 'd28900'];
+        $flag_2_tracker_data = ['color_hex' => '00d219'];
+
+        $flag_1_data = [
+            'label' => 'Старт',
+            'type' => 'start',
+        ];
+        $flag_2_data = [
+            'label' => 'Финиш',
+            'type' => 'finish',
+        ];
+
+        $flag_1_movement = [
+            'latitude' => 27.981056,
+            'longitude' =>  86.895478,
+            'altitude' => 6500,
+            'battery' => 100,
+            'speed' => 0,
+            'bearing' => 0,
+            'generated_at' => '2021-03-11 14:30:00',
+        ];
+
+        $flag_2_movement = [
+            'latitude' => 27.99925,
+            'longitude' =>  86.858,
+            'altitude' => 5300,
+            'battery' => 80,
+            'speed' => 0,
+            'bearing' => 0,
+            'generated_at' => '2021-03-11 14:30:00',
+        ];
+
+        $flag_1_tracker = Tracker::factory(1)->create($flag_1_tracker_data)->first();
+        $flag_2_tracker = Tracker::factory(1)->create($flag_2_tracker_data)->first();
+
+        Flag::factory(1)->create(
+            array_merge(
+                $flag_1_data,
+                ['tracker_id' => $flag_1_tracker->id, 'race_id' => $race->id]
+            )
+        );
+        Flag::factory(1)->create(
+            array_merge(
+                $flag_2_data,
+                ['tracker_id' => $flag_2_tracker->id, 'race_id' => $race->id]
+            )
+        );
+
+        TrackerMovement::factory(1)->create(array_merge(
+            $flag_1_movement,
+            ['tracker_id' => $flag_1_tracker->id],
+        ));
+        TrackerMovement::factory(1)->create(array_merge(
+            $flag_2_movement,
+            ['tracker_id' => $flag_2_tracker->id],
+        ));
     }
 }
